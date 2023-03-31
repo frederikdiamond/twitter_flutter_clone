e4e0fef74c510cd01b32fa16a6ee9bad9b9c7b54
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-04-01 00:20:56 +0200

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '64274c4d91e482075599';
+  static const String projectId = '6427474a115eb576b776';
+  static const String endPoint = 'http://localhost:80/v1';
+}
